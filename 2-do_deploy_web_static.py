7ed10aaddf16d35370fb580c6e9471fe91ffc14e
StevenAwo <rotimiawomodu@gmail.com>
2024-01-11 17:05:16 +0100

    TASK 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Wrote a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy:
+"""
+
+import os
+
+from fabric.api import env
+
+from fabric.api import put, run
+
+env.hosts = ['34.138.245.164', '54.224.201.40']
+
+
+def do_deploy(archive_path):
+    """
+    It help distributes an archive just to your weeb servers
+    """
+    if os.path.exists(archive_path) is False:
+        return(False)
+    try:
+        put(archive_path, '/tmp/')
+        _filename = archive_path.split("/")[-1]
+        file_name = _filename.split(".")[0]
+
+        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
+
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+            (_filename, file_name))
+
+        run('rm /tmp/{}'.format(_filename))
+
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+            .format(file_name))
+        return(True)
+    except:
+        return(False)
