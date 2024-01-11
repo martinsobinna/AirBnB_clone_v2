474afbe99b0c91fb2f34cc3e560a73f5b9726da1
StevenAwo <rotimiawomodu@gmail.com>
2024-01-11 17:20:10 +0100

    TASK 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,20 +26,14 @@ def do_deploy(archive_path):
         file_name = _file_name.split(".")[0]
 
         run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
             (_file_name, file_name))
-
         run('rm /tmp/{}'.format(_file_name))
-
         run('mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/'.format(file_name, file_name))
-
         run('rm -rf /data/web_static/releases/{}/web_static'
             .format(file_name))
-
         run('rm -rf /data/web_static/current')
-
         run('ln -s /data/web_static/releases/{} /data/web_static/current'
             .format(file_name))
         return(True)
